3d4d4c917566e85872c3757e24bfba3cf8bb6ae5
Lexxy <85262576+Snipedprox@users.noreply.github.com>
2022-04-01 14:12:46 -0300

    Add files via upload

---
+++ commands/soundboard/bruh.js
@@ -0,0 +1,16 @@
+module.exports = {
+name: "bruh",
+code: `
+$replaceText[$playTrack[youtube;Bruh sound];$playTrack[youtube;Bruh sound];]
+
+$cooldown[15s;{newEmbed: {title:<a:D_timer:953775784693207070> **Please wait... You are on Cooldown**} {color:$getServerVar[color]} {description: > **You can use the command again in \`%time%\`**
+
+Tip: *While you wait, vote me in [Top.gg](https://top.gg/bot/$clientid)*}}]
+$onlyIf[$voiceID[$authorid]!=0;{newEmbed: {color:$getServerVar[color]} {author:An error has occured:https://cdn.discordapp.com/emojis/946761780540112977.webp?size=56&quality=lossless} {description: <:reddx:953987871319162890> **You need to join a voice channel**}}]
+
+$onlyIf[$voiceID[$authorid]!=undefined;{newEmbed: {color:$getServerVar[color]} {author:An error has occured:https://cdn.discordapp.com/emojis/946761780540112977.webp?size=56&quality=lossless} {description: <:reddx:953987871319162890> **You need to join a voice channel**}}]
+
+$onlyIf[$voiceID[$clientid]!=0;{newEmbed: {color:$getServerVar[color]} {author:An error has occured:https://cdn.discordapp.com/emojis/946761780540112977.webp?size=56&quality=lossless} {description: <:reddx:953987871319162890> **I need to join a voice channel**}}]
+
+$onlyIf[$voiceID[$clientid]!=undefined;{newEmbed: {color:$getServerVar[color]} {author:An error has occured:https://cdn.discordapp.com/emojis/946761780540112977.webp?size=56&quality=lossless} {description: <:reddx:953987871319162890> **I need to join a voice channel**}}]
+`}
