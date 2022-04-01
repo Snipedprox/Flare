3e511df9be1a5002a16978b1fdfc2ca66890fae4
Lexxy <85262576+Snipedprox@users.noreply.github.com>
2022-04-01 14:23:05 -0300

    Add files via upload

---
+++ handler/status.js
@@ -0,0 +1,9 @@
+
+module.exports = (bot) => {
+    bot.status({
+  text: "$numberseparator[$allmemberscount;.] members in $numberseparator[$servercount;.] guilds",
+  type: "WATCHING",
+  status: "dnd",
+  time: 12
+})
+} // you can change it to whatever you like
