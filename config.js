291e2da61d38a8edd4b052bbb3c8471a02270381
Lexxy <85262576+Snipedprox@users.noreply.github.com>
2022-04-01 16:38:45 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     Bot: {
-    token: "OTU3MTk2NjkzMjk4ODk2OTA2.Yj7Q4w.E4rWi0UyOOiFZaVT33SDwoHqnis", //Add your token here
+    token: "xxxxxxxxx", //Add your token here
     prefix: ["$getServerVar[prefix]","<@!$clientID>","<@$clientID>","$getUserVar[uprefix]","$getGlobalUserVar[cprefix]"],
     intents: "all",
     database: {
