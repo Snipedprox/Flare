8dba433c207fbb01692791fe65ea3a57450cb329
Safira <85262576+Snipedprox@users.noreply.github.com>
2022-04-17 14:58:02 -0300

    Delete handler directory

--- handler/status.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = (bot) => {
-    bot.status({
-  text: "$numberseparator[$allmemberscount;.] members in $numberseparator[$servercount;.] guilds",
-  type: "WATCHING",
-  status: "dnd",
-  time: 12
-})
-} // you can change it to whatever you like
